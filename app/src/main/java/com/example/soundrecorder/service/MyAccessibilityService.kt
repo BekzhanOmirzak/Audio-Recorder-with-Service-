76dd6f06a8026022fe96120d487c649b876834e9
Bekjan <bekjan.omirzak98@gmail.com>
2021-10-20 12:35:02 +0600

    Fiveth version

---
+++ app/src/main/java/com/example/soundrecorder/service/MyAccessibilityService.kt
@@ -0,0 +1,21 @@
+package com.example.soundrecorder.service
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class MyAccessibilityService : AccessibilityService() {
+
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+
+    }
+
+    override fun onInterrupt() {
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/soundrecorder/ui/MainActivity.kt
+++ app/src/main/java/com/example/soundrecorder/ui/MainActivity.kt
@@ -3,18 +3,19 @@ package com.example.soundrecorder.ui
 import android.Manifest.permission.*
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
-import android.widget.Button
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.soundrecorder.BuildConfig
-import com.example.soundrecorder.R
-import com.example.soundrecorder.service.AudioRecorderService
+import com.example.soundrecorder.databinding.ActivityMainBinding
 import com.example.soundrecorder.util.TempStorage
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,43 +27,74 @@ class MainActivity : AppCompatActivity() {
     private val PERMISSIONS = arrayOf(
         WRITE_EXTERNAL_STORAGE,
         READ_EXTERNAL_STORAGE,
-        RECORD_AUDIO,
-        MANAGE_EXTERNAL_STORAGE
+        RECORD_AUDIO
     )
 
-    private lateinit var btnStart: Button;
-    private lateinit var btnStop: Button;
+    private var mediaRecorder: MediaRecorder? = null;
+
+    private lateinit var binding: ActivityMainBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
         TempStorage.initSharedPreferences(this);
-        initViews();
         if (TempStorage.getValue())
             audioRecordingRunning(true);
         else
             audioRecordingRunning(false)
 
         checkPermissions(); //Assuming that the  user will the give permissions
-
-        findViewById<Button>(R.id.btn_start).setOnClickListener {
-            Intent(this, AudioRecorderService::class.java).also {
-                it.putExtra("message", "start");
-                startService(it);
-                audioRecordingRunning(true);
-            }
-        }
-
-        findViewById<Button>(R.id.btn_stop).setOnClickListener {
-            Intent(this, AudioRecorderService::class.java).also {
-                it.putExtra("message", "stop")
-                stopService(it);
-                audioRecordingRunning(false);
-            }
-        }
-
         checkStorageAccessPermissionForAndroidR();
 
+        binding.btnStart.setOnClickListener {
+            startAudioRecorder();
+        }
+
+        binding.btnStop.setOnClickListener {
+            stopRecording();
+            audioRecordingRunning(false);
+        }
+
+    }
+
+    private fun startAudioRecorder() {
+        mediaRecorder = MediaRecorder().apply {
+            reset()
+            setAudioSource(MediaRecorder.AudioSource.MIC);
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            setOutputFile(getFilePath())
+            setMaxDuration(5 * 1000)
+            setOnInfoListener { mr, what, extra ->
+                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
+                    Log.e(TAG, "startAudioRecorder: Max Duration has been reached : ")
+                    audioRecordingRunning(false);
+                    stopRecording()
+                }
+            }
+            try {
+                prepare()
+                start();
+                audioRecordingRunning(true);
+            } catch (ex: Exception) {
+                Log.e(TAG, "startAudioRecorder: Exception : ", ex)
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private fun stopRecording() {
+
+        try {
+            mediaRecorder?.let {
+                it.stop();
+                it.release();
+            }
+        } catch (ex: Exception) {
+            Log.e(TAG, "stopRecording: Exception has been thrown :", ex)
+            ex.printStackTrace();
+        }
     }
 
     private fun checkPermissions() {
@@ -71,6 +103,14 @@ class MainActivity : AppCompatActivity() {
                 ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
             }
         }
+    }
+
+
+    private fun getFilePath(): String {
+        var fileName =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).absolutePath;
+        fileName += File.separator + "AUD${System.currentTimeMillis()}.amr"
+        return fileName;
     }
 
     private fun checkStorageAccessPermissionForAndroidR() {
@@ -84,17 +124,18 @@ class MainActivity : AppCompatActivity() {
     private fun audioRecordingRunning(running: Boolean) {
         TempStorage.saveValue(running);
         if (running) {
-            btnStart.isEnabled = false;
-            btnStop.isEnabled = true;
+            binding.btnStart.isEnabled = false;
+            binding.btnStop.isEnabled = true;
         } else {
-            btnStart.isEnabled = true;
-            btnStop.isEnabled = false;
+            binding.btnStart.isEnabled = true;
+            binding.btnStop.isEnabled = false;
         }
     }
 
-    private fun initViews() {
-        btnStart = findViewById(R.id.btn_start);
-        btnStop = findViewById(R.id.btn_stop);
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopRecording();
     }
 
 
